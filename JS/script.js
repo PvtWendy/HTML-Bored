ca63959191c02b526f8df1f3ed996f3af65fc988
Wendy Hemlock <102826495+PvtWendy@users.noreply.github.com>
2023-02-15 20:17:52 -0400

    fixed buttons being clickable when invisible, might make it work better later, works well enough for now

--- JS/script.js
+++ JS/script.js
@@ -1,7 +1,28 @@
+let isRunning = false;
 function showMobileNav() {
-    if (document.getElementById("HeaderRedirects").classList.contains("HeaderRedirectsShow")) {
-        document.getElementById("HeaderRedirects").classList.remove("HeaderRedirectsShow")
-    } else{
-        document.getElementById("HeaderRedirects").classList.add("HeaderRedirectsShow")
+  if (isRunning == false) {
+    if (
+      document
+        .getElementById("HeaderRedirects")
+        .classList.contains("HeaderRedirectsShow")
+    ) {
+      document
+        .getElementById("HeaderRedirects")
+        .classList.remove("HeaderRedirectsShow");
+        isRunning = true
+      setTimeout(() => {
+        document.getElementById("HeaderRedirects").style.display = "none";
+        isRunning = false
+      }, 300);
+    } else {
+      document.getElementById("HeaderRedirects").style.display = "flex";
+      isRunning = true
+      setTimeout(() => {
+        document
+          .getElementById("HeaderRedirects")
+          .classList.add("HeaderRedirectsShow");
+          isRunning = false
+      }, 300);
     }
+  }
 }
