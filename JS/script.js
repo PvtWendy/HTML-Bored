b19160c7dc214c041240abde306ad0dab838f3d9
Wendy Hemlock <102826495+PvtWendy@users.noreply.github.com>
2023-02-15 20:56:56 -0400

    Second attempt at fixing the Redirects problem

--- JS/script.js
+++ JS/script.js
@@ -1,7 +1,16 @@
 function showMobileNav() {
     if (document.getElementById("HeaderRedirects").classList.contains("HeaderRedirectsShow")) {
         document.getElementById("HeaderRedirects").classList.remove("HeaderRedirectsShow")
+        setTimeout(() => {
+            document.getElementById("HeaderRedirects").classList.remove("Show")
+            document.getElementById("HeaderRedirects").classList.add("Hide")
+        }, 300);
     } else{
-        document.getElementById("HeaderRedirects").classList.add("HeaderRedirectsShow")
+        document.getElementById("HeaderRedirects").classList.remove("Hide")
+        setTimeout(() => {
+            document.getElementById("HeaderRedirects").classList.add("Show")
+            document.getElementById("HeaderRedirects").classList.add("HeaderRedirectsShow")
+        }, 300);
+        
     }
 }
