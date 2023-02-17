d4eae836a2698ad33f3acdd4933ef982b0e1540e
Wendy Hemlock <102826495+PvtWendy@users.noreply.github.com>
2023-02-16 21:37:25 -0400

    bad attempt at first content

--- JS/script.js
+++ JS/script.js
@@ -1,16 +1,24 @@
+let isRunning = false;
 function showMobileNav() {
-    if (document.getElementById("HeaderRedirects").classList.contains("HeaderRedirectsShow")) {
-        document.getElementById("HeaderRedirects").classList.remove("HeaderRedirectsShow")
-        setTimeout(() => {
-            document.getElementById("HeaderRedirects").classList.remove("Show")
-            document.getElementById("HeaderRedirects").classList.add("Hide")
-        }, 300);
-    } else{
-        document.getElementById("HeaderRedirects").classList.remove("Hide")
-        setTimeout(() => {
-            document.getElementById("HeaderRedirects").classList.add("Show")
-            document.getElementById("HeaderRedirects").classList.add("HeaderRedirectsShow")
-        }, 300);
-        
+  if (isRunning == false) {
+    if (
+      document.getElementById("HeaderRedirects").classList.contains("HeaderRedirectsShow")
+    ) {
+      document.getElementById("HeaderRedirects").classList.remove("HeaderRedirectsShow");
+      isRunning = true;
+      setTimeout(() => {
+        document.getElementById("HeaderRedirects").classList.remove("Show");
+        document.getElementById("HeaderRedirects").classList.add("Hide");
+        isRunning = false;
+      }, 300);
+    } else {
+      document.getElementById("HeaderRedirects").classList.remove("Hide");
+      isRunning = true;
+      setTimeout(() => {
+        document.getElementById("HeaderRedirects").classList.add("Show");
+        document.getElementById("HeaderRedirects").classList.add("HeaderRedirectsShow");
+        isRunning = false;
+      }, 300);
     }
+  }
 }
